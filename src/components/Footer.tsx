1db38c25f60089ae0c2d522930f16e56126b3d1e
Rafael <rafa.viana.moc@gmail.com>
2023-08-23 16:55:37 -0300

    Footer COmponent created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer()
+{
+    return (
+        <footer className="flex items-center justify-center bg-black text-white w-full h-[200px]">
+            &copy; Atendimento Delivery 2023
+        </footer>
+    );
+}
