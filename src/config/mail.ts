cb72afea77fa602baea01f5c185c8bbb8568f02d
Rafael <rafa.viana.moc@gmail.com>
2023-08-23 17:11:54 -0300

    Service and the config 'created'

---
+++ src/config/mail.ts
@@ -0,0 +1,13 @@
+interface MailConfig{
+    host: string,
+    port: number,
+    user: string,
+    password: string
+}
+
+export default <MailConfig> {
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    user: process.env.MAIL_USERNAME,
+    password: process.env.MAIL_PASSWORD,
+}
---
+++ src/services/mail.ts
@@ -0,0 +1,55 @@
+import Config from '../config/mail';
+
+import { render } from '@react-email/render';
+import React, { ReactElement } from 'react';
+import * as nodemailer from "nodemailer";
+
+interface MailAttachment {
+    filename: string;
+    path?: string;
+    content?: string;
+    contentType?: string;
+}
+
+export default class Mail{
+    constructor(
+        private to?: string,
+        private subject?: string,
+        private message?: string,
+        private attachments?: MailAttachment[],
+    ){}
+
+    async renderMail(Component: ReactElement) {
+        this.message = await render(Component);
+    }
+
+    async sendMail(): Promise<string> 
+    {
+        const transporter = nodemailer.createTransport({
+            host: Config.host,
+            port: Config.port,
+            secure: false,
+            auth: {
+                user: Config.user,
+                pass: Config.password
+            },
+            tls: { rejectUnauthorized: false }
+        });
+
+        const mailOptions = {
+            from: process.env.MAIL_USERNAME,
+            to: this.to,
+            subject: this.subject,
+            html: this.message,
+            attachments: this.attachments,
+        };
+
+        try{
+            const info = await transporter.sendMail(mailOptions);
+            return "E-mail enviado com sucesso!";
+        }catch (error) {
+            console.error(error);
+            throw new Error("Erro ao enviar o e-mail.");
+        }
+    }
+}
