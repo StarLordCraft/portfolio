8c53f46fae81935096f9a51c7c8261cde82b126c
Rafael <rafa.viana.moc@gmail.com>
2023-08-23 16:56:50 -0300

    NavBar Created

---
+++ src/components/NavBard.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Logo from '@/../public/next.svg'
+
+import ResponsiveNavBar from "./ResponsiveNavBar";
+
+function LargeNavBar()
+{
+    return (
+        <nav 
+        className="hidden md:flex items-center justify-around mt-10 px-10 h-full">
+            <h1 className="flex items-center gap-2 font-bold">
+                <Image src={Logo} 
+                width="40" height="40" alt="LOGO" />
+
+                Atendimento Delivery
+            </h1>
+
+            <div className="flex">
+                <Link href="#" className="block mt-4 md:mt-0 md:ml-4 text-slate-600 hover:text-indigo-800">Home</Link>
+                <Link href="#" className="block mt-4 md:mt-0 md:ml-4 text-slate-600 hover:text-indigo-800">Funcionalidades</Link>
+                <Link href="#" className="block mt-4 md:mt-0 md:ml-4 text-slate-600 hover:text-indigo-800">Sobre</Link>
+                <Link href="#" className="block mt-4 md:mt-0 md:ml-4 text-slate-600 hover:text-indigo-800">Parceiros</Link>
+            </div>
+
+            <Link href="#" 
+            className="bg-blue-500 p-2 rounded text-white">
+                Começar Agora
+            </Link>
+        </nav>
+    );
+}
+
+export default function NavBar()
+{
+    return (
+        <header className="fixed w-full top-0 z-50 bg-white">
+            <LargeNavBar /> 
+            <ResponsiveNavBar />     
+        </header>
+    );
+}
