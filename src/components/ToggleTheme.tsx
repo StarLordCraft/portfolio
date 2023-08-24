9eb3189059d2c557d8c2f42ca10fdd3fbe9bdf32
Rafael <rafa.viana.moc@gmail.com>
2023-08-24 13:31:10 -0300

    toggleTheme component is really good

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -14,15 +14,34 @@ export default function ToggleTHeme()
     }, [isDarkTheme]);
 
     return (
-        <button
-        className={`${
-            isDarkTheme ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'
-          } min-h-[40px] min-w-[40px] transition-colors duration-300 rounded-full`}
-            onClick={() => setIsDarkTheme(prevTheme => !prevTheme)}
-        >
-            <span className="material-symbols-outlined">
-                {isDarkTheme? "dark_mode" : "light_mode" }
-            </span>
-        </button>
+    <button
+      className={`${
+        isDarkTheme
+          ? 'bg-gray-800 shadow-sm shadow-white'
+          : 'bg-white shadow-sm shadow-black'
+      } flex items-center justify-evenly min-h-[40px] min-w-[80px] transition-colors duration-300 rounded-full relative`}
+      onClick={() => setIsDarkTheme((prevTheme) => !prevTheme)}
+    >
+      <span
+        className={`material-symbols-outlined relative z-20 
+        ${isDarkTheme ? 'text-white' : 'invisible'
+        }`}
+      >
+        dark_mode
+      </span>
+
+      <span
+        className={`material-symbols-outlined relative z-20 
+        ${isDarkTheme ? 'invisible' : 'text-white'
+        }`}
+      >
+        light_mode
+      </span>
+
+      <div
+        className={`absolute z-10 w-10 h-10 bg-indigo-500 rounded-full ${
+          isDarkTheme ? 'animate-move-left' : 'animate-move-right'
+        }`}></div>
+    </button>
     );
 }
