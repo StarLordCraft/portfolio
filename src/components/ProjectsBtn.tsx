33a36695d0eb0fe265779ddda1f8b83f328f14ee
Rafael <rafa.viana.moc@gmail.com>
2023-08-29 17:03:23 -0300

    changing the projetsBtn

--- src/components/ProjectsBtn.tsx
+++ src/components/ProjectsBtn.tsx
@@ -5,7 +5,7 @@ import { HiArrowRight } from "react-icons/hi2";
 export default function ProjectsBtn()
 {
     return (
-        <div className="mx-auto xl:mx-0 bg-black dark:bg-transparent rounded-full w-[210px] h-[210px] flex items-center justify-center">
+        <div className="mx-auto xl:mx-0 bg-indigo-800 dark:bg-transparent rounded-full w-[210px] h-[210px] flex items-center justify-center">
             <Link href="/work" className="relative w-[200px] h-[200px] flex items-center justify-center
              bg-circleStar bg-cover bg-center bg-no-repeat group z-10"> 
                 <Image src="/rounded-text.png" alt="" 
