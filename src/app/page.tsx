195fe216f8eb0943e7f05e0759561b35b4e4fe77
Rafael <rafa.viana.moc@gmail.com>
2023-08-29 11:44:39 -0300

    testing deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '@/utils/variants';
 
 import ProjectsBtn from '@/components/ProjectsBtn';
+import Animate from '@/components/Animate';
 
 export default function Home() {
   return (
@@ -12,18 +13,37 @@ export default function Home() {
       dark:bg-gradient-to-r dark:from-primaryDark/10 dark:via-black/30 dark:to-black/10'>
         <div className='text-center flex flex-col justify-center xl:pt-40
         xl:text-left h-full container mx-auto'>
-          <h1 className='h1 dark:text-white'>
+
+          <Animate 
+          element={'h1'}
+          animation={'down'} duration={0.2}
+          styles='h1 dark:text-white'>
             Transforming Ideas <br/> Into 
             <span className="text-indigo-500 dark:text-accent"> Digital Reality</span>
-          </h1>
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-black dark:text-white'>
+         </Animate>
+         
+          <Animate 
+          element={'p'}
+          animation={'down'} duration={0.3}
+          styles='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-black dark:text-white'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis laboriosam culpa
             in esse dolorum tempore quam asperiores nemo debitis! Dolor consectetur dignissimos
             molestiae maiores est possimus obcaecati provident ipsa dolore?
-          </p>
-          <div className='flex justify-center xl:hidden relative'>
-            <ProjectsBtn/>
-          </div>
+         </Animate>
+
+          <Animate 
+          element={'div'}
+          animation={'down'} duration={0.4}
+          styles='xl:flex hidden relative'>
+           <ProjectsBtn/>
+         </Animate>
+
+         <Animate 
+         element={'div'}
+         animation={'down'} duration={0.4}
+         styles='flex justify-center xl:hidden relative'>
+           <ProjectsBtn/>
+         </Animate>
         </div>
       </div>
     </section>
