e9d59f3e73eaae5cee7535028999cd9de8726034
Rafael <rafa.viana.moc@gmail.com>
2023-08-29 03:12:03 -0300

    making the ui of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Image from 'next/image'
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/utils/fadeIn';
+
 export default function Home() {
   return (
-    <main className='pt-[80px]'>
-      <section className="relative h-[100vh]">
-        
-      </section>
-    </main>
+    <section className='bg-primaryLight/20 h-full'>
+      <div className='w-full h-full bg-gradient-to-br from-purple-500 to-stone-100
+      dark:bg-gradient-to-r dark:from-primaryDark/10 dark:via-black/30 dark:to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40
+        xl:text-left h-full container mx-auto'>
+          <h1 className='h1 dark:text-white'>
+            Transforming Ideas <br/> Into 
+            <span className="text-indigo-500 dark:text-accent"> Digital Reality</span>
+          </h1>
+          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-black dark:text-white'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis laboriosam culpa
+            in esse dolorum tempore quam asperiores nemo debitis! Dolor consectetur dignissimos
+            molestiae maiores est possimus obcaecati provident ipsa dolore?
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -28,7 +28,7 @@ export function Transition()
           initial="initial" 
           animate="animate"
           exit="exit" 
-          transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}></motion.div>
+          transition={{delay: 0, duration: 0.6, ease: 'easeInOut'}}></motion.div>
          
          <motion.div className='fixed top-0 bottom-0 right-full h-screen z-20
           bg-[#3b2d71]' 
@@ -36,7 +36,7 @@ export function Transition()
           initial="initial" 
           animate="animate"
           exit="exit" 
-          transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}></motion.div>
+          transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}></motion.div>
          
          <motion.div className='fixed top-0 bottom-0 right-full h-screen z-10
           bg-[#4b3792]' 
@@ -44,7 +44,7 @@ export function Transition()
           initial="initial" 
           animate="animate"
           exit="exit" 
-          transition={{delay: 0.6, duration: 0.6, ease: 'easeInOut'}}></motion.div>
+          transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}></motion.div>
         </>
     );
 }
