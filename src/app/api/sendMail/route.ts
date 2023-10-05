da08c892ce26b0a74296a5ecdbbdb33cb6875a6f
Rafael <rafa.viana.moc@gmail.com>
2023-10-05 14:23:46 -0300

    Sending contact logic done!

--- src/app/api/sendMail/route.ts
+++ src/app/api/sendMail/route.ts
@@ -1,7 +1,40 @@
 import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server';
+import Mail from '@/services/mail';
 
-export async function POST(request:NextRequest)
+import Component from 'mails/teste';
+
+export async function POST(request: NextRequest) 
 {
-    return NextResponse.error();
-}
+    try {
+      const res = await request.json();
+      const {email, name, message} = res.body;
+      
+      const contact = new Mail("rafa.dev.moc@gmail.com", "contato", `email de: ${email} mensagem: ${message}`);
+      await contact.sendMail();
+
+      const mail = new Mail(email, `Obrigado pelo contato ${name}`);
+      
+      await mail.renderMail(Component({}));
+      await mail.sendMail();
+      
+      return new NextResponse(
+        JSON.stringify({
+          status: 200,
+          message: 'Email sent successfully',
+          email: mail.message,
+        })
+      );
+
+    } catch (error: any) {
+      console.error('Error sending email:', error.message);
+      
+      return new NextResponse(
+        JSON.stringify({
+          status: 500,
+          message: 'An error occurred while sending the email',
+        })
+      );
+    }
+  }
+  
